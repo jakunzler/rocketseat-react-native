2643c963caa678fc7f493a92c92a4c97a63d651b
Jonas Augusto Kunzler <k.jonasaugusto@gmail.com>
2025-02-14 00:37:11 -0300

    feat: create template projects

---
+++ imhere/src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#992828',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  firstParagraph: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  secondParagraph: {
+    color: '#cdcdcd',
+    fontSize: 20
+  }
+});
